9bfba39caef784ab3a1a17bcd6a166baa27cb0e7
Matthew Mattheus <19mattheus85@gmail.com>
2023-04-25 10:39:37 -0600

    wip

--- src/dao.rs
+++ src/dao.rs
@@ -1,16 +1,26 @@
 use std::{fs, path::Path};
 
-use serde_json::Value;
+use serde_json::{Value, Map};
 
 pub struct Dao {}
 
 impl Dao {
-    pub fn get_serde_value() {
+    pub fn get_serde_value() -> serde_json::Map<std::string::String, Value> {
         
         let path: &Path = Path::new("src/resources/data.json");
         let contents: String =
             fs::read_to_string(path).expect("Should have been able to read the file");
-        return serde_json::from_str(&contents).unwrap();
-        // return &data;
+        let data: Result<Value, serde_json::Error> = serde_json::from_str(&contents);
+        let result: Result<String, Value> = match data {
+            Ok(d) => {
+                let binding = d.as_object().unwrap();
+                return binding.clone()
+            },
+            Err(e) => {
+                let mut binding = Map::new();
+                binding["Error"] = "Sorry, we could not map the data".into();
+                return binding
+            }
+        };
     }
 }
--- src/prompt.rs
+++ src/prompt.rs
@@ -1,73 +1,29 @@
-use std::{fs, path::Path};
+use serde_json::{Value, Map};
 
-use serde_json::Value;
+use crate::dao::Dao;
 
-pub struct Prompt {}
+pub struct Prompt {
+    pub category: String,
+    pub translation: String,
+}
 
 impl Prompt {
-    // todo: abstract logic into new file, possibly "dao.rs"
-    pub fn get_word_for_prompt(self, language: &String) -> (String, String, String, String) {
-        let path: &Path = Path::new("src/resources/data.json");
+    pub fn get_word_for_prompt(self, language: &String) -> String {
+        let data = Dao::get_serde_value();
 
-        let contents: String =
-            fs::read_to_string(path).expect("Should have been able to read the file");
-        let data: Value = serde_json::from_str(&contents).unwrap();
+        let result = get_category(
+            data, &self.category);
 
-        let random_category = find_random_category(&data);
-        let (category_string, value_of_remaining_json) = random_category;
-
-        let random_word = find_random_word(value_of_remaining_json);
-        let (word_string, value_of_remaining_json) = random_word;
-
-        let prompt_word = find_word(value_of_remaining_json, language.to_string());
-
-        return (category_string, word_string, prompt_word, language.to_string());
+        return result;
     }
 }
 
-fn find_random_category<'a>(data: &'a Value) -> (std::string::String, Option<&Value>) {
-    let mut count = 0;
-
-    let map = data.as_object().unwrap();
-    let i = fastrand::usize(..map.len());
-
-    for key in map.keys() {
-        if count == i {
-            return (String::from(key), map.get(key));
+fn get_category(data: Map<String, Value>, desired_category: &str) -> String {
+    for keys in data.keys() {
+        if keys.eq(desired_category) {
+            let result = keys.clone();
+            return result
         }
-        count += 1;
     }
-
-    return (
-        "Something went wrong".to_string(),
-        std::option::Option::None,
-    );
-}
-
-fn find_random_word(data: Option<&Value>) -> (std::string::String, Option<&Value>) {
-    let mut count = 0;
-
-    let temp = Option::expect(data, "Sorry, we couldn't find the value");
-    let map = temp.as_object().unwrap();
-    let i = fastrand::usize(..map.len());
-
-    for key in map.keys() {
-        if count == i {
-            return (String::from(key), map.get(key));
-        }
-        count += 1;
-    }
-
-    return (
-        "Something went wrong".to_string(),
-        std::option::Option::None,
-    );
-}
-
-fn find_word(data: Option<&Value>, type_of_translation: String) -> String {
-    let temp = Option::expect(data, "Sorry, we couldn't find the value");
-
-    let map = temp.as_object().unwrap();
-    let mut prompt_word = Option::expect(map.get(&type_of_translation), "Sorry").as_str();
-    return prompt_word.get_or_insert("I got nothing").to_string();
+    String::from("Sorry, we could not find your category.")
 }
